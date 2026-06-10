466bb914ec0ce1ee63659c702e02e12da6571bd5
Muhammad Mehdi <mehdichangazi135@gmail.com>
2026-06-10 09:51:47 +0500

    category section modified

--- frontend/src/components/landing/LandingCategorizedPublications.tsx
+++ frontend/src/components/landing/LandingCategorizedPublications.tsx
@@ -19,17 +19,18 @@ export default function LandingCategorizedPublications() {
           </p>
         </div>
 
-        <div className="grid grid-cols-5 max-w-6xl mx-auto">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 max-w-6xl mx-auto">
           {TILES.map(({ label, category }) => (
             <Link
               key={label}
               to={categoryPath(category)}
               className="group relative flex items-center justify-between gap-2 px-4 py-5 text-left text-sm text-gray-500 transition-colors hover:text-gray-900
-                         [&:not(:nth-child(5n))]:border-r [&:not(:nth-child(5n))]:border-gray-100
-                         after:absolute after:bottom-0 after:left-4 after:right-4 after:border-b after:border-gray-100
-                         [&:nth-last-child(-n+5)]:after:border-b-0"
+                         border-r border-gray-100 [&:nth-child(2n)]:border-r-0
+                         sm:[&:nth-child(2n)]:border-r sm:[&:nth-child(3n)]:border-r-0
+                         lg:[&:nth-child(2n)]:border-r lg:[&:nth-child(3n)]:border-r lg:[&:nth-child(5n)]:border-r-0
+                         after:absolute after:bottom-0 after:left-4 after:right-4 after:border-b after:border-gray-100"
             >
-              <span className="underline-offset-4 group-hover:underline">{label}</span>
+              <span className="underline-offset-4 group-hover:underline truncate">{label}</span>
               <ArrowUpRight
                 size={14}
                 className="flex-shrink-0 text-gray-600 opacity-0 group-hover:opacity-100 transition-opacity"
